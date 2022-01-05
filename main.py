db754ece84dc4dcc01ffef9ffd57b7f0d2186180
Kyle Martin <kymartin4505@gmail.com>
2022-01-04 22:05:41 -0800

    route index

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # venv activation (bash)
 # source ./env/Scripts/activate
-
-import requests 
+from logging import debug
+from flask import Flask, render_template, request
 from dotenv import dotenv_values
+import requests 
 import base64
 import os 
 import hashlib
+
+from requests.sessions import session
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
 
 config = dotenv_values('.env')
 CLIENT_ID = config.get('CLIENT_ID')
@@ -31,59 +37,68 @@ code_challenge = base64.urlsafe_b64encode(challenge_bytes).rstrip(b'=') # code_c
 rawAuth = f'{CLIENT_ID}:{CLIENT_SECRET}'
 auth = base64.b64encode(rawAuth.encode('ascii')).decode('ascii')
 
+@app.route('/')
 def authUser():
     auth = 'https://accounts.spotify.com/authorize'
-    req_params = {
-        'client_id': CLIENT_ID,
+    payload = {
         'response_type': 'code',
+        'client_id': CLIENT_ID,
         'redirect_uri': REDIRECT_URI,
         'state': 'state',
         'scope': ' '.join(scopes),
         'code_challenge_method': 'S256',
         'code_challenge': code_challenge
     }
-    r = requests.get(auth, params=req_params)
+    r = requests.get(auth, params=payload)
     
     if r.status_code != 200:
         print(f'error: {r.status_code}')
         return r.close()
-    print(r.url) # temp manually get auth url 
+    return render_template('index.html', authUrl=r.url)        
 
-def getToken(code):
-    req_token = 'https://accounts.spotify.com/api/token'
+@app.route('/callback')
+def getToken():
+    code = request.args.get('code')
+    req_token_url = 'https://accounts.spotify.com/api/token'
     header = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': f'Basic {auth}'
     }
-    req_params = {
+    payload = {
         'grant_type': 'authorization_code',
         'code': code,
         'redirect_uri': REDIRECT_URI,
         'client_id': CLIENT_ID,
         'code_verifier': code_verifier
     }
-    r = requests.post(req_token, params=req_params, headers=header)
+    r = requests.post(req_token_url, params=payload, headers=header)
     if r.status_code != 200: 
-        print(f'error: {r.status_code}')
-        return r.close()
-    getToken.access_token = r.json()['access_token'] # retreive access token for future requests
-    getToken.refresh_token = r.json()['refresh_token'] # refresh token for POST /api/token
+        # print(f'error: {r.status_code}')
+        return f'<h1>{r.status_code}</h1>'
+    
+    r.raise_for_status()
+    session['token'] = r.json()['access_token'] 
+    print(session['token'])
+    return redirect('/user')
 
-authUser()
-code = input('paste your auth code from redirect url: ')
-getToken(code)
+@app.route('/user')
+def userPage():
+    return 'Welcome'
+
+# authUser()
+# getToken(code)
 
 def refreshAccessToken():
     refresh_token = 'https://accounts.spotify.com/api/token'
     header = {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
-    req_params = {
+    payload = {
         'grant_type': 'refresh_token',
         'refresh_token': getToken.refresh_token,
         'client_id': CLIENT_ID
     }
-    r = requests.post(refresh_token, params=req_params, headers=header)
+    r = requests.post(refresh_token, params=payload, headers=header)
     if r.status_code != 200: 
         print(f'error: {r.status_code}')
         return r.close()
@@ -91,3 +106,6 @@ def refreshAccessToken():
     refreshAccessToken.new_access_token = r.json()['access_token'] # new access token if requested refresh
     
 # refreshAccessToken()
+
+if __name__ == '__main__':
+    app.run(debug=True)
