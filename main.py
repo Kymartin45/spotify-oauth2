91ea65f3727678859e3c29958112832d4b58b28b
Kyle Martin <kymartin4505@gmail.com>
2022-01-12 16:42:21 -0800

    request top played tracks

--- main.py
+++ main.py
@@ -35,156 +35,192 @@ code_challenge = base64.urlsafe_b64encode(challenge_bytes).rstrip(b'=') # code_c
 rawAuth = f'{CLIENT_ID}:{CLIENT_SECRET}'
 auth = base64.b64encode(rawAuth.encode('ascii')).decode('ascii')
 
-@app.route('/')
-def authUser():
-    auth = 'https://accounts.spotify.com/authorize'
-    payload = {
-        'response_type': 'code',
-        'client_id': CLIENT_ID,
-        'redirect_uri': REDIRECT_URI,
-        'state': 'state',
-        'scope': ' '.join(scopes),
-        'code_challenge_method': 'S256',
-        'code_challenge': code_challenge
-    }
-    r = requests.get(auth, params=payload)
-    
-    if r.status_code != 200:
-        print(f'error: {r.status_code}')
-        return r.close()
-    return render_template('index.html', authUrl=r.url)        
-
-@app.route('/callback')
-def getToken():
-    code = request.args.get('code')
-    req_token_url = 'https://accounts.spotify.com/api/token'
-    header = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': f'Basic {auth}'
-    }
-    payload = {
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': REDIRECT_URI,
-        'client_id': CLIENT_ID,
-        'code_verifier': code_verifier
-    }
-    r = requests.post(req_token_url, params=payload, headers=header)
-    if r.status_code != 200: 
-        return f'<h1>User not authenticated</h1><p>Error code {r.status_code}</p>'
-    
-    r.raise_for_status()
-    getToken.access_token = r.json()['access_token']
-    return redirect('/user')
-    
-@app.route('/user')
-def userPage():
-    user_url = 'https://api.spotify.com/v1/me'
-    header = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {getToken.access_token}'
-    }
-    r = requests.get(user_url, headers=header)
-    if r.status_code != 200:
-        return f'<h1>Error {r.status_code}</h1>'
-    
-    r.raise_for_status()
-    pretty_res = json.loads(r.text)
-    userPage.display_name = r.json()['display_name']
-    userPage.user_id = r.json()['id']
+class spotifyApiHandle:
+    @app.route('/', methods=['GET'])
+    def authUser():
+        auth = 'https://accounts.spotify.com/authorize'
+        payload = {
+            'response_type': 'code',
+            'client_id': CLIENT_ID,
+            'redirect_uri': REDIRECT_URI,
+            'state': 'state',
+            'scope': ' '.join(scopes),
+            'code_challenge_method': 'S256',
+            'code_challenge': code_challenge
+        }
+        r = requests.get(auth, params=payload)
         
-    return render_template(
-        'user.html',
-        title = 'User authenticated', 
-        access_token = getToken.access_token,
-        user_info = json.dumps(pretty_res, indent=2),
-        username = userPage.display_name,
-        user_id = r.json()['id']
-        ) 
+        if r.status_code != 200:
+            print(f'error: {r.status_code}')
+            return r.close()
+        return render_template('index.html', authUrl=r.url)        
 
-# get user playlists
-@app.route('/user/playlists')
-def getUserPlaylists():
-    user_playlist_url = f'https://api.spotify.com/v1/users/{userPage.user_id}/playlists'
-    header = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {getToken.access_token}'
-    }
-    payload = {
-        'limit': 20, 
-        'offset': 0 
-    }
-    r = requests.get(user_playlist_url, params=payload ,headers=header)
-    data = json.loads(r.text)
-    playlists = data['items']
-    
-    playlist_data = []
-    for playlist in playlists:
-        playlist_data.append({
-            'playlist_name': playlist['name'],
-            'playlist_url': playlist['external_urls']['spotify'],
-            'playlist_image': playlist['images'][0]['url'],
-            'playlist_tracks_url': playlist['tracks']['href'],
-            'playlist_id': playlist['id']
-        })
-    
-    map_ids = map(lambda ids: ids['id'], playlists)   
-    ids = list(map_ids)
-    print(ids)
-    
-    # track_url = f'https://api.spotify.com/v1/playlists/{ids[0]}/tracks'
-    # req_track = requests.get(track_url, headers=header)
-    # track_data = json.loads(req_track.text)['items']
-    
-    # TEMPORARY BELOW (WIP) - get tracks among each playlist given their playlist_id {id}
-    tracks = []
-    for id in ids:
-        track_url = f'https://api.spotify.com/v1/playlists/{id}/tracks'
-        req_track = requests.get(track_url, headers=header)
-        track_data = json.loads(req_track.text)['items']
+    @app.route('/callback')
+    def getToken():
+        code = request.args.get('code')
+        req_token_url = 'https://accounts.spotify.com/api/token'
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': f'Basic {auth}'
+        }
+        payload = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'code_verifier': code_verifier
+        }
+        r = requests.post(req_token_url, params=payload, headers=header)
+        if r.status_code != 200: 
+            return f'<h1>User not authenticated</h1><p>Error code {r.status_code}</p>'
+        
+        r.raise_for_status()
+        spotifyApiHandle.getToken.access_token = r.json()['access_token']
+        return redirect('/user')
+        
+    @app.route('/user', methods=['GET'])
+    def userPage():
+        user_url = 'https://api.spotify.com/v1/me'
+        header = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {spotifyApiHandle.getToken.access_token}'
+        }
+        r = requests.get(user_url, headers=header)
+        if r.status_code != 200:
+            return f'<h1>Error {r.status_code}</h1>'
+        
+        r.raise_for_status()
+        pretty_res = json.loads(r.text)
+        spotifyApiHandle.userPage.display_name = r.json()['display_name']
+        spotifyApiHandle.userPage.user_id = r.json()['id']
+            
+        return render_template(
+            'user.html',
+            title = 'User authenticated', 
+            access_token = spotifyApiHandle.getToken.access_token,
+            user_info = json.dumps(pretty_res, indent=2),
+            username = spotifyApiHandle.userPage.display_name,
+            user_id = r.json()['id']
+            ) 
+
+    # get user playlists
+    @app.route('/user/playlists/', methods = ['GET'])
+    def getUserPlaylists():
+        user_playlist_url = f'https://api.spotify.com/v1/users/{spotifyApiHandle.userPage.user_id}/playlists'
+        header = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {spotifyApiHandle.getToken.access_token}'
+        }
+        payload = {
+            'limit': 20, 
+            'offset': 0 
+        }
+        r = requests.get(user_playlist_url, params=payload ,headers=header)
+        data = json.loads(r.text)
+        playlists = data['items']
+        
+        playlist_data = []
+        for playlist in playlists:
+            playlist_data.append({
+                'playlist_name': playlist['name'],
+                'playlist_url': playlist['external_urls']['spotify'],
+                'playlist_image': playlist['images'][0]['url'],
+                'playlist_tracks_url': playlist['tracks']['href'],
+                'playlist_id': playlist['id']
+            })
+        
+        map_ids = map(lambda ids: ids['id'], playlists)   
+        ids = list(map_ids)
+        spotifyApiHandle.getUserPlaylists.playlist_id = ids
+        
+        # track_url = f'https://api.spotify.com/v1/playlists/{ids[0]}/tracks'
+        # req_track = requests.get(track_url, headers=header)
+        # track_data = json.loads(req_track.text)['items']
+        
+        # TEMPORARY BELOW (WIP) - get tracks among each playlist given their playlist_id {id}
+        
+        # tracks = []
+        # for track in track_data:
+        #     tracks.append({
+        #         'track_name': track['track']['name'],
+        #         'track_artist': track['track']['artists'][0]['name']
+        #     })
+        # print(tracks)
+        
+        # map_id = map(lambda playlist : playlist['id'], playlist_data['items'])
+        # playlist_ids = list(map_id)
+                                         
+        return render_template(
+            'userPlaylists.html',
+            username = spotifyApiHandle.userPage.display_name,
+            playlist_data = playlist_data
+            )
+        
+    @app.route('/user/top_tracks', methods=['GET'])
+    def getTopTracks():
+        top_tracks = f'https://api.spotify.com/v1/me/top/tracks'
+        header = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {spotifyApiHandle.getToken.access_token}'
+        }
+        params = {
+            'time_range': 'medium_term' 
+        }
+        r = requests.get(top_tracks, headers=header, params=params)
+        data = json.loads(r.text)
+        track_data = data['items']
+        
+        tracks = []
         for track in track_data:
             tracks.append({
-                'track_name': track['track']['name'],
-                'track_artist': track['track']['artists'][0]['name'],
-                'track_image': track['track']['album']['images'][0]['url'],
-                'track_url': track['track']['external_urls']['spotify'],
-                'track_id': track['track']['id']
+                'track_artist': track['artists'][0]['name'],
+                'track_name': track['name']
             })
-
-    # tracks = []
-    # for track in track_data:
-    #     tracks.append({
-    #         'track_name': track['track']['name'],
-    #         'track_artist': track['track']['artists'][0]['name']
-    #     })
-    # print(tracks)
-    
-    # map_id = map(lambda playlist : playlist['id'], playlist_data['items'])
-    # playlist_ids = list(map_id)
-                                        
-    return render_template(
-        'userPlaylists.html',
-        username = userPage.display_name,
-        playlist_data = playlist_data,
-        tracks = tracks
+            
+        return render_template(
+            'topTracks.html',
+            username = spotifyApiHandle.userPage.display_name,
+            track_data=tracks
         )
+        
+    # @app.route('/user/playlists/', methods=['GET'])
+    # def getPlaylistTracks():
+    #     req_tracks = f'https://api.spotify.com/v1/playlists/{spotifyApiHandle.getUserPlaylists.playlist_id}/tracks'
+    #     header = {
+    #         'Content-Type': 'application/json',
+    #         'Authorization': f'Bearer {spotifyApiHandle.getToken.access_token}'
+    #     }
+    #     r = requests.get(req_tracks, headers=header)
+    #     data = json.loads(r.text)
+    #     track_data = data['items']
+        
+    #     track = []
+    #     for tracks in track_data:
+    #         track.append({
+    #             'track_name': tracks['track']['name']
+    #         })
+    #     print(track)
+        
+    #     return render_template(
+    #         'userPlaylists.html'
+    #     )
 
-def refreshAccessToken():
-    refresh_token = 'https://accounts.spotify.com/api/token'
-    header = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    payload = {
-        'grant_type': 'refresh_token',
-        'refresh_token': getToken.refresh_token,
-        'client_id': CLIENT_ID
-    }
-    r = requests.post(refresh_token, params=payload, headers=header)
-    if r.status_code != 200: 
-        print(f'error: {r.status_code}')
-        return r.close()
-    print(r.json()) 
-    refreshAccessToken.new_access_token = r.json()['access_token'] # new access token if requested refresh
+    def refreshAccessToken():
+        refresh_token = 'https://accounts.spotify.com/api/token'
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        payload = {
+            'grant_type': 'refresh_token',
+            'refresh_token': spotifyApiHandle.getToken.refresh_token,
+            'client_id': CLIENT_ID
+        }
+        r = requests.post(refresh_token, params=payload, headers=header)
+        if r.status_code != 200: 
+            print(f'error: {r.status_code}')
+            return r.close()
+        print(r.json()) 
+        spotifyApiHandle.refreshAccessToken.new_access_token = r.json()['access_token'] # new access token if requested refresh
     
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
