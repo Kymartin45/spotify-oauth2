835ce378c9f0588bd2114422bce796332796e26d
Kyle Martin <kymartin4505@gmail.com>
2022-01-27 16:35:47 -0800

    check route

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,23 @@
 # pytest 6.2.5
 # https://pytest-flask.readthedocs.io/en/latest/
-from main import app as flask_app
+# pytest tests/conftest.py (-m, --verbose)
+
+from main import app
 import pytest
 
-@pytest.fixture
-def app():
-    yield flask_app
+@pytest.mark.testIndex
+def testIndex():
+    client = app.test_client().get('/')
+    assert client.status_code == 200
+    assert client.get_data() 
+
+@pytest.mark.testAuthToken    
+def testAuthToken():
+    client = app.test_client().get('/callback')
+    assert client.status_code == 200
+
+@pytest.mark.testUserPage
+def testUserPage():
+    client = app.test_client().get('/user')
     
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-def testIndex(client):
-    res = client.get('/') 
-    assert res.status_code == 200 
- 
-
+    
