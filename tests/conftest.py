50dfe539250c3e57b06a5db8d4ce9ff712557485
Kyle Martin <kymartin4505@gmail.com>
2022-02-01 12:14:58 -0800

    check login & user page - WIP

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,19 @@
 # https://pytest-flask.readthedocs.io/en/latest/
 # pytest tests/conftest.py (-m, --verbose)
 
-from main import app
+import json
+from unittest import mock
+import main 
 import pytest
+
+app = main.app
 
 @pytest.mark.testIndex
 def testIndex():
     client = app.test_client().get('/')
     assert client.status_code == 200
-    assert client.get_data() 
+    html = client.data.decode()
+    assert 'Login with Spotify' in html 
 
 @pytest.mark.testAuthToken    
 def testAuthToken():
@@ -18,6 +23,12 @@ def testAuthToken():
 
 @pytest.mark.testUserPage
 def testUserPage():
-    client = app.test_client().get('/user')
+    client = app.test_client().get(
+        '/user',
+        content_type = 'application/json',
+        Authorization = 'mock_access_token_1234',
+    )
+    data = json.loads(client.get_data(as_text=True))
+    assert data
     
     
